7c45d83eabb4e4b34c4df49f52c7266ee2e6b836
Katelli <aska082024@gmail.com>
2024-10-30 15:18:34 +0100

    Set ut the Meeting Model

---
+++ Model/Meeting.cs
@@ -0,0 +1,36 @@
+public class Meeting
+{
+    public string Title { get; set; }
+    public string Organizer { get; set; }
+    // TODO: Figure out if this gives DD/MM/YYYY, MM/DD/YYYY, YYYY/MM/DD or YYYY/DD/MM
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public string[] Participants { get; set; }
+
+
+    public Meeting(string title, string organizer, DateOnly date, TimeOnly time, string[] participants)
+    {
+        Title = title;
+        Organizer = organizer;
+        Date = date;
+        Time = time;
+        Participants = participants;
+    }
+
+    public override string ToString()
+    {
+        return $"Meeting: {Title}\nOrganized by: {Organizer}\nDate: {Date}\tTime: {Time}\nParticipants: {Participants}";
+    }
+}
+public class Model
+{
+    public List<Meeting> Meetings {get; set;}
+    public Model()
+    {
+        Meetings = new List<Meeting>
+        {
+            new Meeting("Social Committee", "Donna", DateOnly.Parse("12.12.2025"), new TimeOnly(14,00), ["Joe", "Marie, Jane"]),
+            new Meeting("Executive Meeting", "Frank", DateOnly.Parse("14.05.2025"), new TimeOnly(10,30), ["Lily", "Melody"]),
+        };
+    }
+}
